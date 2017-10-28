2714b20b73eeda8e4807ae9befac57e767d19f28
uym2 <ceciliamaitouyen@gmail.com>
2017-10-27 23:58:26 -0700

    remove redundant files

--- scripts/Backup/reroot_onekp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-reroot.py $1 Monomastix_opisthostigma+Uronema_sp+Nephroselmis_pyriformis+Pyramimonas_parkeae,Chlorokybus_atmophyticus+Spirotaenia_minuta+Mesostigma_viride,Entransia_fimbriata+Klebsormidium_subtile -mrca $2 -igerr
--- scripts/Backup/reroot_xenrs.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-reroot.py $1 Mnemiopsis_leidyi+Amphimedon_queenslandica,Trichoplax_adhaerens,Nematostella_vectensis -mrca $2 -igerr
