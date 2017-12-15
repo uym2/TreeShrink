427ad61d260ff93a15f0b53ffa0e56dc8170f3f9
uym2 <ceciliamaitouyen@gmail.com>
2017-12-15 15:19:53 -0800

    Rlib added so that no installation is needed

--- R_scripts/find_threshold_lkernel.R
+++ R_scripts/find_threshold_lkernel.R
@@ -1,12 +1,14 @@
-suppressMessages(require(BMS))
 
 threshold <- function(y,e=0.05) {x=y[y>0];exp(quantile(density(log(x),adjust=1),p=1-e));}
 
 
 args = commandArgs(TRUE)
 
-datafile = args[1]
-e = as.numeric(args[2])
+libpath = paste(args[1],"/Rlib",sep="")
+datafile = args[2]
+e = as.numeric(args[3])
+
+suppressMessages(require(BMS,lib.loc=libpath))
 
 d = read.table(datafile)
 threshold(d$V1,e=e)
