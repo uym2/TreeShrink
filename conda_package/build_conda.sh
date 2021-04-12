4e886ac18a647c3386f3943e7eb2b8efd37a4b39
Mai To Uyen <uym2@Mais-MBP-3.attlocal.net>
2021-04-11 18:08:20 -0700

    v1.3.9

--- conda_package/build_conda.sh
+++ conda_package/build_conda.sh
@@ -4,5 +4,5 @@ v=$1
 
 sed -i -e "s/version:.*$/version: $v/g" treeshrink/meta.yaml
 conda-build treeshrink/
-conda convert --platform all //anaconda3/conda-bld/osx-64/treeshrink-$v*.tar.bz2 -o //anaconda3/conda-bld/
-anaconda upload -u smirarab --force //anaconda3/conda-bld/*/treeshrink-$v*.tar.bz2
+conda convert --platform all /Users/uym2/anaconda3/conda-bld/osx-64/treeshrink-$v*.tar.bz2 -o /Users/uym2/anaconda3/conda-bld/
+anaconda upload -u smirarab --force /Users/uym2/anaconda3/conda-bld/*/treeshrink-$v*.tar.bz2
