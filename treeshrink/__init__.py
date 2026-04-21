ad489768189eb4edc2329ea3091fc52c065dd861
Uyen Mai <ceciliamaitouyen@gmail.com>
2026-04-21 13:26:44 -0700

    Replaced deprecated tempfile.mktemp with NamedTemporaryFile(delete=False) in treeshrink/__init__.py

--- treeshrink/__init__.py
+++ treeshrink/__init__.py
@@ -15,7 +15,7 @@ PROGRAM_WEBSITE = "https://uym2.github.io/TreeShrink/"
 PROGRAM_INSTITUTE = "Department of Computer Science and Engineering, University of California at San Diego"
 
 
-from tempfile import mkdtemp,mktemp
+from tempfile import mkdtemp,NamedTemporaryFile
 from os.path import basename, dirname, splitext,realpath,join,normpath,isdir,isfile,exists
 from os import mkdir,rmdir
         
@@ -37,10 +37,9 @@ def set_tmp_dir(t):
 def get_tmp_file(name=None,prefix=None):
     global tempdir
     if name is None:
-        if prefix:
-            return mktemp(dir=tempdir,prefix=prefix)
-        else:
-            return mktemp(dir=tempdir)
+        tmp = NamedTemporaryFile(dir=tempdir,prefix=prefix,delete=False)
+        tmp.close()
+        return tmp.name
     else:
         return normpath(join(tempdir,name))
         
